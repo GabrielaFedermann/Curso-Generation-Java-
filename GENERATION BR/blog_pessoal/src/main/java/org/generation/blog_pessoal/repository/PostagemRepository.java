c7be4e8fb39c43753b00ef7a0bfc8c5f0f010330
Gabriela Federmann <gabyfederman@gmail.com>
2022-02-22 20:31:10 -0300

    update

--- GENERATION BR/blog_pessoal/src/main/java/org/generation/blog_pessoal/repository/PostagemRepository.java
+++ GENERATION BR/blog_pessoal/src/main/java/org/generation/blog_pessoal/repository/PostagemRepository.java
@@ -1,14 +1,13 @@
 package org.generation.blog_pessoal.repository;
+
 import java.util.List;
 
-import org.generation.blog_pessoal.model.postagem;
+import org.generation.blog_pessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostagemRepository extends JpaRepository<postagem, Long> {
-	public List<postagem> findAllTituloContaningIgnoreCase (String titulo);
-	
-	
-	
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+
 }
